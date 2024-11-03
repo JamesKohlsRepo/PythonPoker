fb4c9f1dc00bc499a19fdffe5ebf81ac29136dd4
JamesKohlsRepo <jameskohls@live.com>
2024-11-03 12:13:48 -0800

    Added more tests, card dealing logic

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ import random
 from enum import Enum
 
 class GameState(Enum):
-    INIT = 0          
-    DEAL = 1          
-    PRE_FLOP = 2
-    FLOP = 3
-    TURN = 4
-    RIVER = 5
-    SHOWDOWN = 6
-    END = 7
+    INIT = 0                
+    PRE_FLOP = 1
+    FLOP = 2
+    TURN = 3
+    RIVER = 4
+    SHOWDOWN = 5
+    END = 6
 
 class Deck:
     def __init__(self):
         with open('cards.json', 'r') as file:
             self.cards = json.load(file)
+        self.originalDeckSize = len(self.cards)
 
     def shuffle(self):
         random.shuffle(self.cards)
@@ -24,7 +24,7 @@ class Deck:
         """ Returns: (List) cards removed from overall deck """
         delt_cards = []
         for _ in range(number):
-            delt_cards.append(self.cards.pop()) 
+            delt_cards.append(self.cards.pop(0)) 
         return delt_cards
 
 class Player:
@@ -54,29 +54,39 @@ class PokerGame:
                 self.players.remove(player)
                 removed_players.append(player)
             return removed_players
+    
+    def deal_hands(self):
+        """ Deals two cards to each player """
+        for player in self.players:
+            player.hand.extend(self.deck.deal(2))
 
+    def deal_community(self, count):
+        """ Deals community cards"""
+        self.community_cards.extend(self.deck.deal(count))
+
+    def showdown(self):
+        print()
+    
     def advance_state(self):
         if self.current_state == GameState.INIT:
             if len(self.players) >= 2:
-                self.current_state = GameState.DEAL
+                self.deal_hands()
+                #self.post_blinds()
+                self.current_state = GameState.PRE_FLOP
             else:
                 return("error, not enough players")
-        elif self.current_state == GameState.DEAL:
-            self.deal_hands()
-            self.post_blinds()
-            self.current_state = GameState.PRE_FLOP  
         elif self.current_state == GameState.PRE_FLOP:
-            self.deal_flop()
+            self.deal_community(3)
             self.current_state = GameState.FLOP
         elif self.current_state == GameState.FLOP:
-            self.deal_turn()
+            self.deal_community(1)
             self.current_state = GameState.TURN
         elif self.current_state == GameState.TURN:
-            self.deal_river()
+            self.deal_community(1)
             self.current_state = GameState.RIVER
         elif self.current_state == GameState.RIVER:
-            self.current_state = GameState.SHOWDOWN
             self.showdown()
+            self.current_state = GameState.SHOWDOWN
         elif self.current_state == GameState.SHOWDOWN:
             self.current_state = GameState.END
 
--- test_main.py
+++ test_main.py
@@ -1,12 +1,12 @@
 import unittest
-from main import Deck, Player, PokerGame  # Adjust to the actual file/module name
+from main import Deck, Player, PokerGame, GameState  # Adjust to the actual file/module name
 
 class TestDeck(unittest.TestCase):
     def setUp(self):
         self.deck = Deck()  # Create a new deck for each test
 
     def test_deck_initialization(self):
-        self.assertEqual(len(self.deck.cards), 52)  # Assuming a standard deck size
+        self.assertEqual(len(self.deck.cards), self.deck.originalDeckSize) 
 
     def test_shuffle(self):
         original_order = self.deck.cards[:]
@@ -14,10 +14,10 @@ class TestDeck(unittest.TestCase):
         self.assertNotEqual(original_order, self.deck.cards)  # Order should be different
 
     def test_deal(self):
-        card = self.deck.deal(1)  # Deal one card, returns a list with one item
-        self.assertIsInstance(card, list)  # Expect a list
-        self.assertEqual(len(card), 1)  # List should contain exactly one card
-        self.assertIsInstance(card[0], dict)  # The first (and only) item should be a dictionary
+        card = self.deck.deal(1)  
+        self.assertIsInstance(card, list)  
+        self.assertEqual(len(card), 1)  
+        self.assertIsInstance(card[0], dict)  
         self.assertIn('suit', card[0])
         self.assertIn('value', card[0])
 
@@ -62,7 +62,44 @@ class TestPokerGameState(unittest.TestCase):
     def setUp(self):
         self.game = PokerGame()
         self.player1 = Player("user1")
+        self.player2 = Player("user2")
+        self.player3 = Player("user3")
+        self.player4 = Player("user4")
 
+    def _fourPlayerGame(self):
+        self.game.addPlayers(self.player1, self.player2, self.player3, self.player4)
+        self.game.deck.shuffle()
+
+    def test_notEnoughPlayers(self):
+        self.game.addPlayers(self.player1)
+        self.assertEqual(self.game.advance_state(), "error, not enough players")
+
+    def test_enoughPlayersStartGame(self):
+        self.game.addPlayers(self.player1, self.player2)
+        self.game.advance_state()
+        self.assertEqual(self.game.current_state, GameState.PRE_FLOP)
+
+    def test_dealtwocards(self):
+        self._fourPlayerGame()
+        self.game.advance_state()
+        counter = 0
+        for player in self.game.players:
+            self.assertEqual(len(player.hand), 2) 
+            counter += 2
+        self.assertEqual(len(self.game.deck.cards), (self.game.deck.originalDeckSize-counter)) 
+
+    def test_communityCardDeals(self):
+        self._fourPlayerGame()
+        self.game.advance_state()
+        self.game.advance_state()
+        self.assertEqual(self.game.current_state, GameState.FLOP)
+        self.assertEqual(len(self.game.community_cards), 3)
+        self.game.advance_state()
+        self.assertEqual(self.game.current_state, GameState.TURN)
+        self.assertEqual(len(self.game.community_cards), 4)
+        self.game.advance_state()
+        self.assertEqual(self.game.current_state, GameState.RIVER)
+        self.assertEqual(len(self.game.community_cards), 5)
 
 if __name__ == '__main__':
     print("\nBEGIN TESTING")
